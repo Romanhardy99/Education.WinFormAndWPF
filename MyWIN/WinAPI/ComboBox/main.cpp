2542c436c8eb97670a5201318fc6004d21ac9c77
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2025-12-25 20:33:16 +0400

    ComboBox add task

--- MyWIN/WinAPI/ComboBox/main.cpp
+++ MyWIN/WinAPI/ComboBox/main.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<windows.h>
+#include<stdio.h>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "Combo", "Box" };
@@ -36,18 +38,18 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDOK:
 			{
 				HWND hCombo = GetDlgItem(hwnd, IDC_COMBO); 
-				int index = (int)SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-				if (index == CB_ERR)
+				INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+				if (i == CB_ERR)
 				{
-					MessageBox(hwnd, "Ничего не выбрано", "info", MB_OK | MB_ICONWARNING);
+					MessageBox(hwnd, "ичего не выбрано", NULL, MB_OK | MB_ICONERROR);
 					break;
 				}
-
-				char buffer[256] = {};
-				SendMessage(hCombo, CB_GETLBTEXT, index, (LPARAM)buffer);
-
-				MessageBox(hwnd, buffer, "В поле выбрано: ", MB_OK | MB_ICONINFORMATION);
-				break;
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				CHAR sz_message[SIZE] = {};
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+				sprintf(sz_message, "Вы выбрали элемент #%i со значением %s", i, sz_buffer);
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 			}
 		}
 
